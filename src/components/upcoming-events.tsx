c73e5f8fc00132f7f1102db2f7eb01e6beba1646
D0dii <marcindolatowski123@gmail.com>
2024-12-27 17:53:03 +0100

    fix: main page title

--- src/components/upcoming-events.tsx
+++ src/components/upcoming-events.tsx
@@ -28,22 +28,25 @@ const UpcomingEvents = () => {
     return <LoadingSkeleton />;
   }
   return upcomingEvents.length === 0 ? (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Description</TableHead>
-          <TableHead>Created at</TableHead>
-          <TableHead className="text-right">Amount</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <TableRow>
-          <TableCell colSpan={3} className="h-14 text-center text-xl">
-            {"You don't have any upcoming events"}
-          </TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
+    <div className="flex w-full flex-col gap-1">
+      <h2 className="text-3xl">Upcoming events</h2>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Description</TableHead>
+            <TableHead>Created at</TableHead>
+            <TableHead className="text-right">Amount</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell colSpan={3} className="h-14 text-center text-xl">
+              {"You don't have any upcoming events"}
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
   ) : (
     <div className="flex w-full flex-col gap-1">
       <h2 className="text-3xl">Upcoming events</h2>
