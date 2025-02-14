276e9ef4877abf78d486b3f5b9e85a50a88b01d1
D0dii <marcindolatowski123@gmail.com>
2025-02-14 16:23:07 +0100

    chore: unnecessary comment, slug in schema

--- src/auth.ts
+++ src/auth.ts
@@ -6,21 +6,6 @@ import prisma from "@/lib/db";
 
 import authConfig from "./auth.config";
 
-// declare module "next-auth" {
-//   interface Session {
-//     user: PrismaUser & DefaultSession["user"];
-//   }
-//   interface User extends PrismaUser {}
-// }
-
-// declare module "next-auth/jwt" {
-//   interface JWT {
-//     id: string;
-//     email: string;
-//     username: string;
-//   }
-// }
-
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   session: {
